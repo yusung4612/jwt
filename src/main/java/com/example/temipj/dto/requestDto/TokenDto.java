b39e3467cdf8e401896eafad9b14fcf2267b7b70
ysjeong <ysjeong@everybot.net>
2023-03-10 17:31:10 +0900

    feat : 토큰에 담기는 값들 추가

--- src/main/java/com/example/temipj/dto/requestDto/TokenDto.java
+++ src/main/java/com/example/temipj/dto/requestDto/TokenDto.java
@@ -1,5 +1,6 @@
 package com.example.temipj.dto.requestDto;
 
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -21,10 +22,10 @@ public class TokenDto { //서버에서 발급받은 토큰 정보를 저장
     private Long accessTokenExpiresIn; //토큰 만료시간 정보
 
     // 토큰에 담기는 값
-//    public void tokenToHeaders(HttpServletResponse response) {
-//        response.addHeader("Authorization", "Bearer " + getAccessToken()); //엑세스 토큰
-//        response.addHeader("Refresh_Token", getRefreshToken()); //리프레시 토큰
-//        response.addHeader("Access-Token-Expire-Time", getAccessTokenExpiresIn().toString()); //엑세스토큰 만료시간
-//    }
+    public void tokenToHeaders(HttpServletResponse response) {
+        response.addHeader("Authorization", "Bearer " + getAccessToken()); //엑세스 토큰
+        response.addHeader("Refresh_Token", getRefreshToken()); //리프레시 토큰
+        response.addHeader("Access-Token-Expire-Time", getAccessTokenExpiresIn().toString()); //엑세스토큰 만료시간
+    }
 
 }
