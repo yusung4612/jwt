b8d19332249180810afd3bbf8661418dec57ade9
ysjeong <ysjeong@everybot.net>
2023-03-14 09:35:48 +0900

    feat : EmpResponseDto 생성

--- src/main/java/com/example/temipj/dto/responseDto/EmpResponseDto.java
+++ src/main/java/com/example/temipj/dto/responseDto/EmpResponseDto.java
@@ -1,4 +1,29 @@
 package com.example.temipj.dto.responseDto;
 
-public class EmpResponseDto {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class EmpResponseDto<T> {
+
+    private String version;
+    private T member;
+//    private Error error;
+
+    public static <T> EmpResponseDto<T> version(T data) {
+//        return new ResponseDto<>(1, data, null);
+        return new EmpResponseDto<>("1.0.0", data);
+    }
+
+    public static <T> EmpResponseDto<T> fail(String code, String message) {
+        return new EmpResponseDto<>("Error", null);
+    }
+
+    @Getter
+    @AllArgsConstructor
+    static class Error {
+        private String code;
+        private String message;
+    }
 }
