7133eda0ff1e4a2ab3e73b99ffa340b0a24aa5b1
ysjeong <ysjeong@everybot.net>
2023-03-15 19:38:05 +0900

    delete : 필요없는 feild 삭제

--- src/main/java/com/example/temipj/dto/requestDto/LeaderRequestDto.java
+++ src/main/java/com/example/temipj/dto/requestDto/LeaderRequestDto.java
@@ -9,14 +9,19 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class LeaderRequestDto {
 
-    private String division; //구분
+    private Long id;
 
-    private String department; //부서
-
-    private String name; //이름
-
-    private String mobile_number; //폰번호
-
-    private String email; //이메일
-
+//    private String division; //구분
+//
+//    private String department; //부서
+//
+//    private String name; //이름
+//
+//    private String mobile_number; //폰번호
+//
+//    private String email; //이메일
+//
+//      private Member member;
+//
+//      private Employee employee;
 }
