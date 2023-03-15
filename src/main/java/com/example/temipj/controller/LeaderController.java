15d0cc217b4612d65186adac8379a55d55d19dc1
ysjeong <ysjeong@everybot.net>
2023-03-15 19:36:46 +0900

    fix : Service명 및 메서드 변경

--- src/main/java/com/example/temipj/controller/LeaderController.java
+++ src/main/java/com/example/temipj/controller/LeaderController.java
@@ -1,11 +1,9 @@
 package com.example.temipj.controller;
 
-import com.example.temipj.domain.UserDetailsImpl;
 import com.example.temipj.dto.responseDto.ResponseDto;
-import com.example.temipj.service.LeaderCheckService;
+import com.example.temipj.service.LeaderService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,30 +11,17 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api")
 public class LeaderController {
 
-    private final LeaderCheckService leaderCheckService;
+    private final LeaderService leaderService;
 
-    // 리더 임명
-    @PostMapping("/leaders/{employeeId}")
-    public ResponseDto<?> LeaderCheck(@PathVariable Long employeeId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        return leaderCheckService.LeaderCheck(employeeId, userDetails);
+    // 리더 지정
+    @PostMapping("/leaders/check/{employeeId}")
+    public ResponseDto<?> createLeader(@PathVariable Long employeeId, HttpServletRequest request) {
+        return leaderService.LeaderCheck(employeeId, request);
     }
 
-    //리더 전체 조회
-    @GetMapping("/leaders/all")
-    public ResponseDto<?> getLeaderAll() {
-        return leaderCheckService.getLeaderAll();
+    //리더 전체 목록 조회
+    @GetMapping("/leaders")
+    public ResponseDto<?> getLeaderAll(HttpServletRequest request) {
+        return leaderService.getLeaderAll(request);
     }
-
-
-    //리더 생성
-//    @PostMapping(value = "/create")
-//    private LeadResponseDto<?> create(@RequestBody LeaderRequestDto leaderRequestDto,
-//                                      HttpServletRequest request) {
-//        return leaderService.createLeader(leaderRequestDto, request);
-//    }
-//
-//    @GetMapping(value = "/all")
-//    public LeadResponseDto<?> getLeaderAll(){
-//        return leaderService.getLeaderAll();
-//    }
 }
