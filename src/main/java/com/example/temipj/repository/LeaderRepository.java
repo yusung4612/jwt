2c601a7223e5f420406db0217bced3c57b32c026
ysjeong <ysjeong@everybot.net>
2023-03-17 18:15:40 +0900

    feat : 검색 쿼리 추가

--- src/main/java/com/example/temipj/repository/LeaderRepository.java
+++ src/main/java/com/example/temipj/repository/LeaderRepository.java
@@ -1,23 +1,30 @@
 package com.example.temipj.repository;
 
+import com.example.temipj.domain.Employee;
 import com.example.temipj.domain.Leader;
 import com.example.temipj.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
-@Repository
-@EnableJpaRepositories
 public interface LeaderRepository extends JpaRepository<Leader, Long> {
 
     Optional<Leader> findById(Long id);
 
     List<Leader> findAllByMember(Member member);
 
+    boolean existsByMemberAndEmployee(Member member, Employee employee);
+
     //리더 체크
     Leader findByEmployeeIdAndMemberId(Long memberId, Long employeeId);
 
+//    @Query(value = "SELECT p FROM Employee p WHERE p.name LIKE %:keyword% OR p.birth LIKE %:keyword% " +
+//            "OR p.division LIKE %:keyword% OR p.extension_number LIKE %:keyword% OR p.mobile_number LIKE %:keyword% " +
+//            "OR p.email LIKE %:keyword% OR p.department LIKE %:keyword% ORDER BY p.createdAt desc")
+//    List <Employee> search(@Param("keyword") String keyword);
 }
