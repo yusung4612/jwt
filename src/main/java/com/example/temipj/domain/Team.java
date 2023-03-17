d5a87da9968e9669ae7775fbe565cb9d0e0e711b
ysjeong <ysjeong@everybot.net>
2023-03-17 18:19:38 +0900

    feat : Timestamped 상속

--- src/main/java/com/example/temipj/domain/Team.java
+++ src/main/java/com/example/temipj/domain/Team.java
@@ -15,7 +15,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-public class Team {
+public class Team extends Timestamped {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
