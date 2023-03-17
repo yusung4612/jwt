2ad393d40ec6cdfa20b1c0bb053ffd97829841d3
ysjeong <ysjeong@everybot.net>
2023-03-17 18:17:34 +0900

    feat : 파라미터 필드명 변경

--- src/main/java/com/example/temipj/dto/responseDto/Leader/LeadResponseDto.java
+++ src/main/java/com/example/temipj/dto/responseDto/Leader/LeadResponseDto.java
@@ -10,8 +10,8 @@ public class LeadResponseDto<T> {
     private String version;
     private T division;
 
-    public static <T> LeadResponseDto<T> version(T division) {
-        return new LeadResponseDto("230331", division);
+    public static <T> LeadResponseDto<T> version(T data) {
+        return new LeadResponseDto("230331", data);
     }
 
 //    @Getter
