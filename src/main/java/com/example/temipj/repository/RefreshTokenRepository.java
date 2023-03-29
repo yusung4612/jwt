6f235bb3c05cd86e4cc6645f2be66678ad3fd602
ysjeong <ysjeong@everybot.net>
2023-03-29 16:27:22 +0900

    fix : 코드 정렬

--- src/main/java/com/example/temipj/repository/RefreshTokenRepository.java
+++ src/main/java/com/example/temipj/repository/RefreshTokenRepository.java
@@ -14,4 +14,5 @@ public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long
     Optional<RefreshToken> findByValue(String value);
 
     Optional<RefreshToken> deleteByAdminId(Long id);
+
 }
