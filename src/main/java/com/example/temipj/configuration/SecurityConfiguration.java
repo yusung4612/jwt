6ed10e8079f03d9babe84e7affe12ca8ae8800a1
ysjeong <ysjeong@everybot.net>
2023-03-29 16:28:36 +0900

    feat : 프론트 연결을 위한 requestMatchers 추가

--- src/main/java/com/example/temipj/configuration/SecurityConfiguration.java
+++ src/main/java/com/example/temipj/configuration/SecurityConfiguration.java
@@ -14,6 +14,7 @@ import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -84,11 +85,10 @@ public class SecurityConfiguration {
                         .requestMatchers(AUTH_WHITELIST).permitAll()
                         .requestMatchers("/api/**").hasRole("ROLE_ADMIN")
                         .requestMatchers("/api/**").permitAll()
-//                        .requestMatchers("/api/admins/login").permitAll()
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                        .requestMatchers("/api/admins/**").permitAll()
                         .requestMatchers("/", "/**").permitAll()
                         .requestMatchers("/v2/api-docs",
-//                                "/swagger-resources",
-//                                "/swagger-resources/**",
                                 "/configuration/ui",
                                 "/configuration/security",
                                 "/swagger-ui.html",
