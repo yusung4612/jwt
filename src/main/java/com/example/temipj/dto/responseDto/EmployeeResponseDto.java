93a296c734b9988efaf875f6b9acf1c2d3ceffbf
ysjeong <ysjeong@everybot.net>
2023-03-29 16:19:11 +0900

    fix : 주석 수정

--- src/main/java/com/example/temipj/dto/responseDto/EmployeeResponseDto.java
+++ src/main/java/com/example/temipj/dto/responseDto/EmployeeResponseDto.java
@@ -13,15 +13,15 @@ public class EmployeeResponseDto {
 
 //    private Long id;
 
-    private String name; //직원 이름
+    private String name; // 직원 이름
 
-    private String birth; //직원 생일
+    private String birth; // 직원 생일
 
     private String division; // 팀 구분
 
-    private String extension_number; //무선 전화 번호
+    private String extension_number; // 무선 전화 번호
 
-    private String mobile_number; //모바일번호
+    private String mobile_number; // 모바일번호
 
     private String enabled;
 
