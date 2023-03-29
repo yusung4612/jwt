c0c86310fb70cf8ce12d025bd3502229ee8428ee
ysjeong <ysjeong@everybot.net>
2023-03-29 16:16:13 +0900

    feat : 코드 정렬

--- src/main/java/com/example/temipj/repository/DivisionRepository.java
+++ src/main/java/com/example/temipj/repository/DivisionRepository.java
@@ -1,6 +1,5 @@
 package com.example.temipj.repository;
 
-import com.example.temipj.domain.employee.Department;
 import com.example.temipj.domain.employee.Division;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -11,6 +10,5 @@ public interface DivisionRepository extends JpaRepository<Division, Long> {
     List<Division> findAllByOrderByCreatedAtDesc();
 
     Division findById(String divisionId);
-
 
 }
