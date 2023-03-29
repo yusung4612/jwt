367d2b25beda37db0da8b369314d1ea91c2c3a22
ysjeong <ysjeong@everybot.net>
2023-03-29 16:25:59 +0900

    feat : 각 필드 조건 관련 주석 추가

--- src/main/java/com/example/temipj/dto/requestDto/NewsRequestDto.java
+++ src/main/java/com/example/temipj/dto/requestDto/NewsRequestDto.java
@@ -9,8 +9,14 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class NewsRequestDto {
 
+    //    @NotBlank(message = "팀명을 입력해주세요.")
+//    @Size(min = 1, max = 10, message = "최소 1자이상 최대 20자미만으로 입력해주세요.")
+//    @Pattern(regexp = "[a-zA-Zㄱ-ㅎ가-힣]*${1,20}", message = "이름 형식을 확인해 주세요.")
     private String message; // 뉴스 메세지
 
+    //    @NotBlank(message = "팀명을 입력해주세요.")
+//    @Size(min = 1, max = 10, message = "최소 1자이상 최대 20자미만으로 입력해주세요.")
+//    @Pattern(regexp = "[a-zA-Zㄱ-ㅎ가-힣]*${1,20}", message = "이름 형식을 확인해 주세요.")
     private String author; // 작성자
 
 }
