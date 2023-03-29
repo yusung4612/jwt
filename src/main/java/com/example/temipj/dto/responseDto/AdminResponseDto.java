a0711f556710f0cadf5bdc3092b368309221db97
ysjeong <ysjeong@everybot.net>
2023-03-29 16:06:19 +0900

    fix : 주석 수정

--- src/main/java/com/example/temipj/dto/responseDto/AdminResponseDto.java
+++ src/main/java/com/example/temipj/dto/responseDto/AdminResponseDto.java
@@ -15,11 +15,11 @@ public class AdminResponseDto {
 
     private Long id;
 
-    private String adminName; //이름
+    private String adminName; // 이름
 
-    private String emailId; //이메일
+    private String emailId; // 이메일
 
-    private LocalDateTime createdAt; //생성시간
+    private LocalDateTime createdAt; // 생성시간
 
-    private LocalDateTime modifiedAt; //수정시간
+    private LocalDateTime modifiedAt; // 수정시간
 }
