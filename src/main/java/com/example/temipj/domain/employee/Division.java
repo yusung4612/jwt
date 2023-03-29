30d34197f854b1b0e903bc012fe571370ae24fd2
ysjeong <ysjeong@everybot.net>
2023-03-29 16:14:20 +0900

    feat : @JoinColumn 제거

--- src/main/java/com/example/temipj/domain/employee/Division.java
+++ src/main/java/com/example/temipj/domain/employee/Division.java
@@ -21,14 +21,4 @@ public class Division extends Timestamped {
     @Column(nullable = false)
     private String division; // 팀 이름
 
-//    @JoinColumn(nullable = false)
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    private Admin admin; // join 해야하나?
-
-
-
-//    @JoinColumn(nullable = false)
-//    @OneToMany(fetch = FetchType.LAZY)
-//    private List<Employee> employee;
-
 }
